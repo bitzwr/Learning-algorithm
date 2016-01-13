793bf598f5af24879fbf4c6a3fb53b0e8ee2ea7d
Wang Biao <bitzwr@gmail.com>
2016-01-13 23:33:51 +0800

    插入排序

--- src/org/wangbiao/algo/sort/Insertion.java
+++ src/org/wangbiao/algo/sort/Insertion.java
@@ -20,7 +20,7 @@ public class Insertion {
      * INSERTION-SORT(A)
      *   for i ← 2 to length[A]
      *     do key ← A[i]
-     *       △ Insert A[i] into the sorted sequence A[1..i-1].
+     *       ▷ Insert A[i] into the sorted sequence A[1..i-1].
      *       j ← i - 1
      *       while j > 0 and A[j] > key
      *         do A[j + 1] ← A[j]
