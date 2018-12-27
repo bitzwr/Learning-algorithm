aff7c38b5739dc9c889ad5b1cf51908076c73487
Bitzwr Wang <bitzwr@gmail.com>
2018-12-27 21:26:28 +0800

    朴素的模式匹配算法

---
+++ algorithm/src/main/java/org/wangbiao/algo/stringmatch/NaiveStringMatcher.java
@@ -0,0 +1,34 @@
+package org.wangbiao.algo.stringmatch;
+
+/**
+ * 朴素字符串匹配算法。
+ */
+public class NaiveStringMatcher {
+
+    /**
+     *
+     * @param T text
+     * @param P pattern
+     * @return 匹配到text中第一个pattern的索引,没有的话返回-1
+     */
+    public static int match(String T, String P) {
+        int n = T.length();
+        int m = P.length();
+        for (int i = 0; i < n - m; i++) {
+            // 对比T[i, i + m - 1] 和 P 是否匹配
+            for (int j = 0; j < m; j++) {
+                if (T.charAt(i + j) != P.charAt(j)) {
+                    break;
+                }
+                if (j == m - 1) { // 为true 说明每一位都对比过了
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        match("qabcababca", "abcab");
+    }
+}
