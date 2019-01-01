554de236e39a335bfe8f76cba75262c283955e0c
Bitzwr Wang <bitzwr@gmail.com>
2019-01-01 19:49:59 +0800

    双链表LRU

---
+++ leetcode/src/main/java/org/wangbiao/leetcode/$146LRUCache.java
@@ -0,0 +1,117 @@
+package org.wangbiao.leetcode;
+
+public class $146LRUCache {
+
+    public static class LRUCache {
+        private int capacity;
+        private int size;
+
+        private Node head;
+
+        private static class Node {
+            Node pre;
+            Node next;
+            int key;
+            int value;
+
+            Node(int key, int value) {
+                this.key = key;
+                this.value = value;
+            }
+
+            void remove() {
+               this.pre.next = this.next;
+               this.next.pre = this.pre;
+            }
+
+            void  addBefore(Node node) {
+                this.next = node;
+                this.pre = node.pre;
+                this.pre.next = this;
+                this.next.pre = this;
+            }
+
+            void access(LRUCache cache) {
+                this.remove();
+                this.addBefore(cache.head);
+            }
+
+        }
+
+        public LRUCache(int capacity) {
+            this.capacity = capacity;
+            head = new Node(-1, -1);
+            head.pre = head.next = head; // 哨兵，减化边界判断
+        }
+
+        public int get(int key) {
+            Node node = head.next;
+            while (node != null && node != head) {
+                if (node.key == key) {
+                    node.access(this);
+                    return node.value;
+                } else {
+                    node = node.next;
+                }
+            }
+            return -1;
+        }
+
+        public void put(int key, int value) {
+            Node temp = head.next;
+            while (temp != null && temp != head) { // 先查找
+                if (temp.key == key) {
+                    temp.value = value;
+                    temp.access(this);
+                    return;
+                } else {
+                    temp = temp.next;
+                }
+            }
+            Node node = new Node(key, value);
+            node.addBefore(head);
+            size++;
+            if (size > capacity) {
+                head.next.remove();
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+
+        cache.put(1, 1);
+        System.out.println(cache.get(1));
+        System.out.println(cache.toString());
+        cache.put(2, 2);
+
+        int i = cache.get(1);
+        System.out.println(i);
+        System.out.println(cache.toString());
+
+        cache.put(3, 3);
+        System.out.println(cache.toString());
+
+        int i1 = cache.get(2);
+        System.out.println(i1);
+        System.out.println(cache.toString());
+
+        cache.put(4, 4);
+        System.out.println(cache.toString());
+        int i2 = cache.get(1);
+        System.out.println(i2);
+        System.out.println(cache.toString());
+
+        int i3 = cache.get(3);
+        System.out.println(i3);
+        System.out.println(cache.toString());
+
+        int i4 = cache.get(4);
+        System.out.println(i4);
+        System.out.println(cache.toString());
+
+    }
+
+}
